8250fdedb4bee054e2b8cf9fc90a8a3fc46f0494
Carlos <cadudrop@hotmail.com>
2021-07-24 09:17:14 -0300

    Organizando arquivos

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
+//Primeira coisa a fazer em todos os componentes React
+import React from 'react';
 
+const App = () => {
+  const message = 'Hello world!';
+
+  return <h1>{message}</h1>;
+}
+
+export default App;//pq o index.js importa o App, aí a gente precisa exportar
